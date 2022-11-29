0ecc5d8062dbec70b5b48bb9be30dfac7070dbc8
kikswosil <kikswosil@wp.pl>
2022-11-29 10:26:31 +0100

    database works now. next thing to do is Dockerize.

--- app/app.py
+++ app/app.py
@@ -1,59 +1,54 @@
 from flask import Flask
-from flask import render_template
-import db
+from flask import render_template, request, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 
-# FIXME: remove this variable when the db queries are in place.
-
-messages = [
-    {
-        "id": 0,
-        "title": "first item",
-        "details": "lorem ipsum"
-    },
-    {
-        "id": 1,
-        "title": "second item",
-        "details": "dolor sit"
-    },
-    {
-        "id": 2,
-        "title": "third item",
-        "details": "amet consecteur",
-    }
-]
-
-
-# FIXME: remove this function when the db queries are in place.
-
-
-def findItemById(item_id=None):
-    global messages
-
-    for item in messages:
-        if item["id"] == item_id:
-            return item
-
-    # return error if the item wasn't found.
-    return {
-        "id": -1,
-        "title": "error",
-        "details": "failed to find item with given id."
-    }
-
+import config
 
 app = Flask(__name__)
-db.init_app(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = config.DB_URL
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):  # type: ignore
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    details = db.Column(db.String(300), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/delete/<int:item_id>')
+def delete(item_id):
+    db.session.delete(Todo.query.get_or_404(item_id))
+    db.session.commit()
+    return redirect('/')
+
+
+@app.route('/create/', methods=('GET', 'POST'))  # type: ignore
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        details = request.form['details']
+        if not title:
+            return flash('Title is required.')
+        if not details:
+            return flash('Details are required.')
+        db.session.add(Todo(title=title, details=details))
+        db.session.commit()
+
+        return redirect('/')
+    return render_template("create.html")
 
 
 @app.route("/")
 def index():
-    # TODO: replace this with a database query.
-    global messages
-    return render_template("main.html", items=messages)
+    items = Todo.query.all()
+    return render_template("main.html", items=items)
 
 
 @app.route("/details/<int:item_id>")
 def details(item_id):
-    # TODO: replace this with a database query.
-    item = findItemById(item_id)
+    item = Todo.query.get_or_404(item_id)
     return render_template("details.html", item=item)
